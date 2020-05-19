e2c9dcb569fbfc99074ec61c589568360a577b61
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2020-05-19 17:24:59 -0300

    Save photo

--- src/pages/Cam/index.js
+++ src/pages/Cam/index.js
@@ -1,19 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Alert } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import * as Permissions from 'expo-permissions';
+
 import CameraButton from '../../components/CameraButton';
 import { ViewCamera, ViewButtonCamera } from './styles';
-import { Camera } from 'expo-camera';
 import AlbumCamera from '../../components/AlbumCamera';
 
 export default function Cam() {
 
     const [hasPermission, setHasPermission] = useState(null);
+    const [hasPermissionRegister, setHasPermissionRegister] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
+
+    const [camera, setCamera] = useState(null);
 
     useEffect(() => {
         (async () => {
             const { status } = await Camera.requestPermissionsAsync();
+            const { statusRegister } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
             setHasPermission(status === 'granted');
+            setHasPermissionRegister(statusRegister === 'granted');
         })();
     }, []);
 
@@ -23,9 +31,29 @@ export default function Cam() {
     if (hasPermission === false) {
         return <Text>No access to camera</Text>;
     }
+
+    async function takePictureAndCreateAlbum() {
+        const { uri } = await camera.takePictureAsync();
+        const asset = await MediaLibrary.createAssetAsync(uri);
+        const album = await MediaLibrary.getAlbumAsync('Agita Santos');
+        if(!album){
+            await MediaLibrary.createAlbumAsync('Agita Santos', asset).then(() => {
+                Alert.alert('Create Album');
+            }).catch((err) => {
+                Alert.alert(err);
+            });
+        }else{
+            await MediaLibrary.addAssetsToAlbumAsync(asset, album).then(() =>{
+                Alert.alert('Save photo Album');
+            }).catch((err) => {
+                Alert.alert(err);
+            });
+        }
+    }
+
     return (
         <ViewCamera>
-            <Camera style={{ flex: 1 }} type={type}>
+            <Camera style={{ flex: 1 }} type={type} ref={event => setCamera(event)}>
                 <View
                     style={{
                         flex: 1,
@@ -45,7 +73,12 @@ export default function Cam() {
                                 );
                             }}
                         />
-                        <CameraButton size={100} icon='md-camera' sizeIcon={40} />
+                        <CameraButton
+                            size={100}
+                            icon='md-camera'
+                            sizeIcon={40}
+                            onPress={() => takePictureAndCreateAlbum()}
+                        />
                         <AlbumCamera />
                     </ViewButtonCamera>
                 </View>
